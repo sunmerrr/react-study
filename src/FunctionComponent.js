13c339f9efc805d7b8403925ecfa53e8a27dc869
BILW <sunmer6@gmail.com>
2023-04-08 03:08:29 +0900

    TypeProps의 isRequired

--- src/FunctionComponent.js
+++ src/FunctionComponent.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types'; // react v15.5부터 외부 라이브러리로 이동
 
-export const FunctionComponent = ({ name, children }) => {
+export const FunctionComponent = ({ name, favoriteNumber, children }) => {
   return (
     <div>
-      {name} 함수형 컴포넌트
-      <br /> children : {children}
+      {name} 함수형 컴포넌트 공부하기
+      <br />
+      children : {children}
+      <br />
+      가장 좋아하는 숫자 : {favoriteNumber}
     </div>
   );
 };
@@ -14,4 +17,5 @@ FunctionComponent.defaultProps = { name: 'react' };
 
 FunctionComponent.propTypes = {
   name: PropTypes.string,
+  favoriteNumber: PropTypes.number.isRequired, // 필수로 받아와야 하는 값으로 지정
 };
