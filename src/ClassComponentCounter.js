4d87138e789abfbc873df44cf7c21cb092a43518
BILW <sunmer6@gmail.com>
2023-04-08 15:17:39 +0900

    class component 여러 state 값

--- src/ClassComponentCounter.js
+++ src/ClassComponentCounter.js
@@ -9,17 +9,20 @@ export class ClassComponentCounter extends Component {
     // 객체 형식이어야 함
     this.state = {
       number: 0,
+      fixedNumber: 2,
     };
   }
   render() {
-    const { number } = this.state; // state는 this.state로 조회 가능
+    const { number, fixedNumber } = this.state; // state는 this.state로 조회 가능
     return (
       <div>
         <h1>{number}</h1>
+        <h2>바뀌지 않는 값: {fixedNumber}</h2>
         <button
           // 버튼 클릭시 일어날 이벤트를 설정해줌
           onClick={() => {
-            // this.setState를 통해서 state값을 변경
+            // this.setState를 통해서 state값을 변경 가능
+            // 인자로 전달된 객체 안에 들어있는 값만 바꿔 줌
             this.setState({ number: number + 1 });
           }}
         >
