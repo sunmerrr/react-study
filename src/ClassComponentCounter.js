49d2c01512d28d235d41bb49d26235504f59622e
BILW <sunmer6@gmail.com>
2023-04-08 15:29:23 +0900

    setState에 객체 대신 함수 인자 전달

--- src/ClassComponentCounter.js
+++ src/ClassComponentCounter.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
 
 export class ClassComponentCounter extends Component {
-  // 컴포넌트에 state를 설정할때 constructor에 메서드를 작성하여 설정해야 함.
-  constructor(props) {
-    // super() 함수를 통해서 현재 컴포넌트가 상속받고 있는 리액트의 Component 클래스가 지닌 생성자 함수를 호출해줌
-    super(props);
-    // state의 초기값 설정
-    // 객체 형식이어야 함
-    this.state = {
-      number: 0,
-      fixedNumber: 2,
-    };
-  }
+  // // 컴포넌트에 state를 설정할때 constructor에 메서드를 작성하여 설정해야 함.
+  // constructor(props) {
+  //   // super() 함수를 통해서 현재 컴포넌트가 상속받고 있는 리액트의 Component 클래스가 지닌 생성자 함수를 호출해줌
+  //   super(props);
+  //   // state의 초기값 설정
+  //   // 객체 형식이어야 함
+  //   this.state = {
+  //     number: 0,
+  //     fixedNumber: 2,
+  //   };
+  // }
+
+  // constructor를 사용하지 않고도 state값을 선언해 줄 수 있다.
+  state = {
+    number: 0,
+    fixedNumber: 2,
+    plusPrevNumber: 0,
+  };
+
   render() {
-    const { number, fixedNumber } = this.state; // state는 this.state로 조회 가능
+    const { number, fixedNumber, plusPrevNumber } = this.state; // state는 this.state로 조회 가능
     return (
       <div>
         <h1>{number}</h1>
@@ -27,6 +35,18 @@ export class ClassComponentCounter extends Component {
           }}
         >
           +1
+        </button>
+
+        <h1>{plusPrevNumber}</h1>
+        <button
+          onClick={() => {
+            // 객체 대신에 함수인자를 전달하여 클릭 이벤트를 설정할 수 있음
+            this.setState((prevState) => {
+              return { plusPrevNumber: prevState.plusPrevNumber + 1 };
+            });
+          }}
+        >
+          plus using previous number +1
         </button>
         <br />
         <br />
