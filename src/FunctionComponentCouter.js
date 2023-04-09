d104ba6db43f244b35ee1460be349cf15a64f8b1
BILW <sunmer6@gmail.com>
2023-04-09 11:19:50 +0900

    function component useState

--- src/FunctionComponentCouter.js
+++ src/FunctionComponentCouter.js
@@ -2,13 +2,22 @@ import React, { useState } from 'react';
 
 export const FunctionComponentCouter = () => {
   const [number, setNumber] = useState(0);
-  const [fixedNumber, setFixedNumber] = useState(2);
+  const [fixedNumber] = useState(2);
+  const [plusPrevNumber, setPlusPrevNumber] = useState(0);
 
   return (
     <div>
       <h1>{number}</h1>
       <h2>바뀌지 않는 값: {fixedNumber}</h2>
       <button onClick={() => setNumber(number + 1)}>+1</button>
+      <h1>{plusPrevNumber}</h1>
+      <button
+        onClick={() => {
+          setPlusPrevNumber((prev) => prev + 1);
+        }}
+      >
+        plus using previous number +1
+      </button>
       <br />
       <br />
       <br />
