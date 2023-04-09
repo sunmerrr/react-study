8a953b72a6ffa86696bd7b0cbe342b53d903e904
BILW <sunmer6@gmail.com>
2023-04-09 11:31:40 +0900

    여러 state

--- src/FunctionComponentSayHi.js
+++ src/FunctionComponentSayHi.js
@@ -2,12 +2,12 @@ import React, { useState } from 'react';
 
 export const FunctionComponentSayHi = () => {
   const [message, setMessage] = useState('');
+  const [color, setColor] = useState('black');
 
   return (
     <div>
       <hr />
-      <h1>{message}</h1>
-
+      <h1 style={{ color }}>{message}</h1>
       <button
         onClick={() => {
           setMessage('Hi there!');
@@ -21,6 +21,16 @@ export const FunctionComponentSayHi = () => {
         }}
       >
         퇴장
+      </button>
+      <h2>인사 메세지 컬러를 바꿉니다.</h2>
+      <button style={{ color: 'green' }} onClick={() => setColor('green')}>
+        green
+      </button>
+      <button style={{ color: 'blue' }} onClick={() => setColor('blue')}>
+        blue
+      </button>
+      <button style={{ color: 'black' }} onClick={() => setColor('black')}>
+        black
       </button>
     </div>
   );
