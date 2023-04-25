f83396b121fecb9b446c4c66d7d098c846eba818
BILW <sunmer6@gmail.com>
2023-04-25 12:55:55 +0900

    delete

--- src/components/Map/FunctionMap.js
+++ src/components/Map/FunctionMap.js
@@ -10,21 +10,31 @@ const FunctionMap = () => {
   const [inputText, setInputText] = useState('');
   const [nextId, setNextId] = useState(5);
 
-  const nameList = names.map((name, index) => <li key={name.id}>{name.text}</li>);
+  const nameList = names.map((name, index) => (
+    <>
+      <li key={name.id}>{name.text}</li>
+      <button onClick={() => hanldeDelete(name.id)}>삭제</button>
+    </>
+  ));
 
   const handleChange = (e) => setInputText(e.target.value);
 
-  const handleButtonClick = () => {
-    const addName = names.concat({ id: nextId, text: inputText });
-    setNames(addName);
+  const handleAdd = () => {
+    const newNames = names.concat({ id: nextId, text: inputText });
+    setNames(newNames);
     setNextId(nextId + 1);
     setInputText('');
+  };
+
+  const hanldeDelete = (id) => {
+    const newNames = names.filter((name) => name.id !== id);
+    setNames(newNames);
   };
 
   return (
     <div>
       <input type="text" value={inputText} onChange={handleChange} />
-      <button onClick={handleButtonClick}>추가</button>
+      <button onClick={handleAdd}>추가</button>
       <ul>{nameList}</ul>
     </div>
   );
