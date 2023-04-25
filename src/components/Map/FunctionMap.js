b0283db8fa99d06465430418aa61ca024a430be9
BILW <sunmer6@gmail.com>
2023-04-25 12:48:32 +0900

    add event

--- src/components/Map/FunctionMap.js
+++ src/components/Map/FunctionMap.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import { useState } from 'react';
 
 const FunctionMap = () => {
-  const names = ['snowman', 'ice', 'snow', 'wind'];
-  const nameList = names.map((name, index) => <li key={index}>{name}</li>);
+  const [names, setNames] = useState([
+    { id: 1, text: 'snowman' },
+    { id: 2, text: 'ice' },
+    { id: 3, text: 'snow' },
+    { id: 4, text: 'wind' },
+  ]);
+  const [inputText, setInputText] = useState('');
+  const [nextId, setNextId] = useState(5);
+
+  const nameList = names.map((name, index) => <li key={name.id}>{name.text}</li>);
+
+  const handleChange = (e) => setInputText(e.target.value);
+
+  const handleButtonClick = () => {
+    const addName = names.concat({ id: nextId, text: inputText });
+    setNames(addName);
+    setNextId(nextId + 1);
+    setInputText('');
+  };
 
   return (
     <div>
+      <input type="text" value={inputText} onChange={handleChange} />
+      <button onClick={handleButtonClick}>추가</button>
       <ul>{nameList}</ul>
     </div>
   );
