53b604e2cf0efc05453b957c0ded664d9c7ef7c8
BILW <sunmer6@gmail.com>
2023-05-10 19:53:11 +0900

    useReducer로 counter 기능 구현

---
+++ src/components/Hooks/Reducer.js
@@ -0,0 +1,28 @@
+import { useReducer } from 'react';
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'INCREMENT':
+      return { value: state.value + 1 };
+    case 'DECREMENT':
+      return { value: state.value - 1 };
+    default:
+      return state;
+  }
+}
+
+const Reducer = () => {
+  const [state, dispatch] = useReducer(reducer, { value: 0 });
+
+  return (
+    <div>
+      <p>
+        reducer 현재 카운터 값: <b>{state.value}</b>
+      </p>
+      <button onClick={() => dispatch({ type: 'INCREMENT' })}> + 1</button>
+      <button onClick={() => dispatch({ type: 'DECREMENT' })}> - 1</button>
+    </div>
+  );
+};
+
+export default Reducer;
