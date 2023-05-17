87e26130f62bad8aeedcee20af3fbc469751e205
BILW <sunmer6@gmail.com>
2023-05-17 18:51:17 +0900

    state example

--- src/components/Hooks/State.js
+++ src/components/Hooks/State.js
@@ -1,25 +1,47 @@
 import { useState } from 'react';
 
-const testFuntion = () => {
-  console.log('initialize state');
-  return 'state setting';
-};
 const State = () => {
   const [number, setNumber] = useState(0);
+  const [name, setName] = useState('');
+  const [date, setDate] = useState(Date());
 
-  const handleClick = () => {
-    setNumber(number + 1);
-    console.log('state를 스크린에 그려지기 전에 호출함 = 효과 적용 안됨', number);
+  const handleNumber = () => {
+    setNumber(1);
+    console.log('number set 함수를 호출', number);
+  };
+
+  const handleName = () => {
+    setName('여름');
+    console.log('name set 함수를 호출', name);
+  };
+
+  const handleDate = () => {
+    setDate(Date());
+    console.log('date set 함수를 호출', date);
   };
 
   return (
     <div>
       <p>
         현재 숫자: <b>{number}</b>
+        <br />
+        이름: <b>{name}</b>
+        <br />
+        시간: <b>{date}</b>
       </p>
-      {console.log('state를 스크린에 그려진 이후에 호출함 = 원하는 효과가 적용됨', number)}
-      <button onClick={handleClick}>+ 1</button>
-      <button onClick={() => setNumber(number - 1)}>- 1</button>
+      {console.log('초기 렌더링과 리렌더링 시 함께 호출되는 콘솔 로그', number)}
+      <button
+        onClick={() => {
+          handleNumber();
+          handleName();
+          handleDate();
+        }}
+      >
+        적용
+      </button>
+      <br />
+      <br />
+      <br />
       <br />
       <br />
       <br />
