5b1e933ea180f28ba2180529af8d423eb0afdad4
BILW <sunmer6@gmail.com>
2023-05-20 14:58:37 +0900

    add function app

--- src/FunctionApp.js
+++ src/FunctionApp.js
@@ -7,6 +7,9 @@ import FunctionRef from './components/Ref/FunctionRef';
 import FunctionMap from './components/Map/FunctionMap';
 import State from './components/Hooks/State';
 import HookContainer from './components/Hooks/HookContainer';
+import ReducerCounter from './components/Hooks/ReducerCounter';
+import ReducerInfo from './components/Hooks/ReducerInfo';
+import Memo from './components/Hooks/Memo';
 
 function FunctionApp() {
   return (
@@ -16,7 +19,15 @@ function FunctionApp() {
       <h1>Built-in Hooks</h1>
       <State />
       <HookContainer />
-
+      <ReducerCounter />
+      <ReducerInfo />
+      <Memo />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
       <hr />
       <h1>컴포넌트 반복</h1>
       <FunctionMap />
