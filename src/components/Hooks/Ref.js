0d98c6eb6ef618af100c42ce3f8b13f91a533331
BILW <sunmer6@gmail.com>
2023-06-02 09:22:38 +0900

    add useRef example

--- src/components/Hooks/Ref.js
+++ src/components/Hooks/Ref.js
@@ -5,12 +5,11 @@ const Ref = () => {
   // Ref는 초기값을 가진 current 속성만을 returen 함
   // 다음 렌더링에서도 같은 값을 return 함, ref는 state 처럼 변경하거나 읽어올 수 있지만 ref를 변경한다고 해서 리렌더링이 일어나지는 않음
   const intervalRef = useRef(0);
-  const inputRef = useRef(null);
 
   const handleStartClick = () => {
     const intervalId = setInterval(() => {
       console.log('seted interval');
-    }, 1000);
+    }, 300);
     intervalRef.current = intervalId;
   };
 
