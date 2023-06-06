0f7fcb96ff6d67eea4a2f2be46bf169a711213c0
BILW <sunmer6@gmail.com>
2023-06-07 08:49:34 +0900

    example of wrapping effect in custom hooks

--- src/components/Hooks/Effect.js
+++ src/components/Hooks/Effect.js
@@ -1,30 +1,29 @@
-import { useState } from 'react';
-import ModalDialog from './ModalDialog';
+import { useState, useEffect } from 'react';
+import { useEventListener } from './useEventListener';
 
 const Effect = () => {
-  const [show, setShow] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  useEventListener('pointermove', (e) => {
+    setPosition({ x: e.clientX, y: e.clientY });
+    console.log('e.clientX: ', e.clientX, 'e.clientY: ', e.clientY);
+  });
 
   return (
-    <div>
-      <button
-        onClick={() => {
-          setShow(true);
-        }}
-      >
-        Open dialog
-      </button>
-      <ModalDialog isOpen={show}>
-        Hi There!
-        <br />
-        <button
-          onClick={() => {
-            setShow(false);
-          }}
-        >
-          Close dialog
-        </button>
-      </ModalDialog>
-    </div>
+    <div
+      style={{
+        position: 'absolute',
+        backgroundColor: 'pink',
+        borderRadius: '50%',
+        opacity: 0.6,
+        transform: `translate(${position.x}px, ${position.y}px)`,
+        pointerEvents: 'none',
+        left: -20,
+        top: -20,
+        width: 40,
+        height: 40,
+      }}
+    ></div>
   );
 };
 
